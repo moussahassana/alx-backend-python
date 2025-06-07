1126aaa21c1306938786df4e7326218b1b901c1f
Moussa Hassana <moussahassana42@gmail.com>
2025-06-07 23:55:57 +0100

    feat(auth-settings): add basic and session based auth

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -87,6 +87,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'chats.auth.CustomJWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
