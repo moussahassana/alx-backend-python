1b134e4e3ea5d0eaa53357db6bdae964b540c13f
Moussa Hassana <moussahassana42@gmail.com>
2025-06-15 21:47:44 +0100

    fix(messaging): Add views to complete thread implementation

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     # URL for the user's inbox (a list of all conversations)
+     path('inbox/', views.inbox, name='inbox'),
+
+     # URL to view a specific conversation thread
+     path('thread/<int:thread_id>/', views.message_thread_view, name='message_thread'),
+    
+     # URL to handle posting a reply to a thread
+     path('thread/<int:thread_id>/reply/', views.send_reply, name='send_reply'),
      path('account/delete/', views.delete_user, name='delete_account'),
 ]
